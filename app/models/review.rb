49c8b4e5936645072f5001dc5c19c616b04e5726
Roman <romtega88@gmail.com>
2015-12-09 16:15:58 -0600

    Added Scoring using Raty

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
 	validates_presence_of :content
+	validates_presence_of :score
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209033318) do
+ActiveRecord::Schema.define(version: 20151209212725) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20151209033318) do
     t.string   "user_id"
     t.string   "integer"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "score",      default: 0
   end
 
   create_table "users", force: :cascade do |t|
